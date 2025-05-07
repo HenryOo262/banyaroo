344203aefc18cd6d7cca1580e4bbb4ffd9147ff6
Henry Oo <oohenry225@gmail.com>
2025-05-07 07:41:51 +0630

    ui

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 from mysite import create_app
-from werkzeug.middleware.dispatcher import DispatcherMiddleware
     
-app = create_app()
-
-def handler(event, context):
-    return app(event, context)
-
 if __name__ == '__main__':
+
+    app = create_app()
 
     app.run(debug=True)
